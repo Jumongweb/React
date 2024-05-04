d050f0cb4f5de13d385a002a0f9e213b05c4e213
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-05-04 12:40:24 +0100

    first react app

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,20 @@
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx"
+import Food from "./Food.jsx"
+import Text from "./Text.jsx"
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Text/>
+      <Food/>
+      <Food/>
+      <Footer/>
+    </>
+  
+  )
+}
+
+export default App
---
+++ my-react-app/src/Text.jsx
@@ -0,0 +1,6 @@
+function Text(){
+    const text = "Learning React is amazing";
+    return text;
+}
+
+export default Text;
